4ba6e4c74631dd75943ee4cc4ec78b9562652c1b
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-07-13 09:25:58 -0600

    Create class in index.js to handle nav buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Everything is Fine!");
+import "./css/style.css";
+
+class userMenuInterface {
+  constructor() {
+    this.homeBtn = document.querySelector("#home-btn");
+    this.menuBtn = document.querySelector("#menu-btn");
+    this.aboutBtn = document.querySelector("#about-btn");
+    this.orderBtn = document.querySelector("#order-btn");
+  }
+}
